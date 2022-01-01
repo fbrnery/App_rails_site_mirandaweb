4358ac85b4a685a321bf319b82045eb9743d8bb4
Fabricio Miranda <fbrnery@gmail.com>
2022-01-01 19:39:51 -0300

    Add pages aplicações front e aplicações back

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'home/sobrenos'
   get 'home/portfolio'
   get 'home/contatos'
+  get 'home/appfront'
+  get 'home/appback'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
