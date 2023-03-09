5f13a997580247b860ed8dd17cf5ddb955369beb
Fabricio Miranda <fbrnery@gmail.com>
2023-03-09 18:10:08 -0300

    Atualização geral no site

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,13 @@ Rails.application.routes.draw do
   get 'home/sobrenos'
   get 'home/portfolio'
   get 'home/contatos'
-  get 'home/appfront'
+  get 'home/sites'
   get 'home/appruby'
+  get 'home/ecommerces'
+  get 'home/cruds'
+  get 'home/midias_sociais'
+  get 'home/google'
+  get 'home/criacao_conteudo'
   get 'contatos/new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
